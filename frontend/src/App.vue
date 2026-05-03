eaf7d66ae15dd759a3c371cb96cbe542ef640a0f
CowRules <roskeats@gmail.com>
2026-05-03 23:09:33 +0300

    Add navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,12 +4,32 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="wrapper text-center">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div class="container">
+        <div class="navbar-brand">MinMax Macros</div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#MinMaxMacrosNavbar"
+          aria-controls="MinMaxMacrosNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="MinMaxMacrosNavbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/" active-class="active">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/about" active-class="active">About</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
 
   <div class="container text-center mt-5">
