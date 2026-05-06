49c4189d618416c6a83eac2a7b7c115367f2460f
CowRules <roskeats@gmail.com>
2026-05-06 21:54:44 +0300

    frontend: update About page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>About MinMax Macros</h1>
   </div>
+  <p><b>Welcome to MinMax Macros.</b> This is a hobby project created by a single person.
+    The purpose of this project is to help people who struggle with finding food supplies with the <b>highest protein</b>,
+    <b>lowest calorie</b> amount or just the <b>best price</b>.</p>
 </template>
